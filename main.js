c67f8398916fe49bcdc05d73fb86b35802ac6e46
Hoorain644 <hoorainhaqnawaz60@gmail.com>
2026-05-20 22:42:47 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+menu = document.querySelector('.menu i');
+navbar = document.querySelector('.header .navbar');
+
+menu.onclick=()=>{
+    navbar.classList.toggle('active');
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
